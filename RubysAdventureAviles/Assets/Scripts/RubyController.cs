e97bf0807fa4f8b65d5944cb3c23c2229f892c96
Andresav776 <715867@student.dusd.net>
2022-11-01 11:43:01 -0700

    Created my first tile
    Created my first tile of the tilemap, waiting to figure out how to properly assign sprite.

--- RubysAdventureAviles/Assets/Scripts/RubyController.cs
+++ RubysAdventureAviles/Assets/Scripts/RubyController.cs
@@ -17,8 +17,8 @@ public class RubyController : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = transform.position;
-        position.x = position.x + 4.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 4.0f * vertical * Time.deltaTime;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
