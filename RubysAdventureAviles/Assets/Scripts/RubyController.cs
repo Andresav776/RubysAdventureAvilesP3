688e963830bfe67a45d1991ed7070ed42a3697e1
Andresav776 <715867@student.dusd.net>
2022-11-18 11:43:21 -0800

    Added colliders and added collectible
    Added colliders to water tiles.  Fixed Ruby and box colliders.  Added a health collectable.

--- RubysAdventureAviles/Assets/Scripts/RubyController.cs
+++ RubysAdventureAviles/Assets/Scripts/RubyController.cs
@@ -4,27 +4,48 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
+    public float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Variables that are pre-built in unity that calibrate movement controls appropriately
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical"); 
+    }
 
+    void FixedUpdate()
+    {
         //Setting Movement Vector
         Vector2 position = transform.position;
 
         //Transforming position
-        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         //Setting position
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
